c844f0c7117c27a640511a41d2eaeb57212e857e
Josh Henrich <jhenrich003@gmail.com>
2018-03-08 15:47:31 -0600

    Update config.pp

--- modules/wordpress/manifests/config.pp
+++ modules/wordpress/manifests/config.pp
@@ -1,12 +1,12 @@
 class wordpress::config {
       $db_name          = 'wordpress-db'
       $db_user          = 'wordpress@%'
-	  $wp_user          = 'wordpress'
+      $wp_user          = 'wordpress'
       $db_password      = 'wordpress'
       $db_host          = 'dbserver'
       $db_phrase        = 'testphrase'
       $root_password    = 'bk3qql'
       $db_perms         = 'ALL'
       $db_tables        = "${db_name}.*"
-	  $db_ip            = '172.31.0.10'	  
+      $db_ip            = '172.31.0.10'	  
 }
